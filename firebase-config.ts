776f4c94ca9dbeeaa680897cf786a1eea4c75173
erentaskiran <101654590+erentaskiran@users.noreply.github.com>
2024-02-06 13:17:00 +0300

    Commit mesajı buraya yazılır

--- firebase-config.ts
+++ firebase-config.ts
@@ -1,9 +1,11 @@
+import { env } from "process";
+
 export const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
-  };
+  apiKey: env.FIREBASE_API_KEY,
+  authDomain: env.FIREBASE_AUTH_DOMAIN,
+  projectId: env.FIREBASE_PROJECT_ID,
+  storageBucket: env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.FIREBASE_APP_ID,
+  measurementId: env.FIREBASE_MEASUREMENT_ID,
+};
